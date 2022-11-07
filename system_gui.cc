27dff965f7cbb50c1ebc8536d46d084df64558ba
Jose Luis Rivero <jrivero@osrfoundation.org>
2022-11-08 00:31:46 +0100

    Create custom plugin for printing onAddEntity
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- system_gui.cc
+++ system_gui.cc
@@ -22,25 +22,15 @@
 
 namespace gazebo
 {
-  class SystemGUI : public SystemPlugin
+  class MyEventPlugin : public SystemPlugin
   {
-    /////////////////////////////////////////////
-    /// \brief Destructor
-    public: virtual ~SystemGUI()
-    {
-      this->connections.clear();
-      if (this->userCam)
-        this->userCam->EnableSaveFrame(false);
-      this->userCam.reset();
-    }
-
     /////////////////////////////////////////////
     /// \brief Called after the plugin has been constructed.
     public: void Load(int /*_argc*/, char ** /*_argv*/)
     {
-      this->connections.push_back(
-          event::Events::ConnectPreRender(
-            std::bind(&SystemGUI::Update, this)));
+      this->_onAddEntityConnection =
+          event::Events::ConnectAddEntity( [ this ]( auto added ) { onAddEntity( added ); }
+        );
     }
 
     /////////////////////////////////////////////
@@ -50,40 +40,15 @@ namespace gazebo
     }
 
     /////////////////////////////////////////////
-    /// \brief Called every PreRender event. See the Load function.
-    private: void Update()
+    private: void onAddEntity( std::string added )
     {
-      if (!this->userCam)
-      {
-        // Get a pointer to the active user camera
-        this->userCam = gui::get_active_camera();
-
-        // Enable saving frames
-        this->userCam->EnableSaveFrame(true);
-
-        // Specify the path to save frames into
-        this->userCam->SetSaveFramePathname("/tmp/gazebo_frames");
-      }
-
-      // Get scene pointer
-      rendering::ScenePtr scene = rendering::get_scene();
-
-      // Wait until the scene is initialized.
-      if (!scene || !scene->Initialized())
-        return;
-
-      // Look for a specific visual by name.
-      if (scene->GetVisual("ground_plane"))
-        std::cout << "Has ground plane visual\n";
+      gzwarn << "Activated onAddEntity event with name: " << added << std::endl;
     }
 
-    /// Pointer the user camera.
-    private: rendering::UserCameraPtr userCam;
-
     /// All the event connections.
-    private: std::vector<event::ConnectionPtr> connections;
+    private: event::ConnectionPtr _onAddEntityConnection;
   };
 
   // Register this plugin with the simulator
-  GZ_REGISTER_SYSTEM_PLUGIN(SystemGUI)
+  GZ_REGISTER_SYSTEM_PLUGIN(MyEventPlugin)
 }
